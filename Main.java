38c1e0b9e354b08ff768da741736481d4f84c7a0
sashaefits@gmail.com <sashaefits@gmail.com>
2025-07-25 20:55:33 +0700

    tv1

--- Main.java
+++ Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+class Tv {
+    private String model; //объявление поля класса
+    private double size; //объявление поля класса
+    private int volume; //объявление поля класса
+
+    public Tv(String model, double size, int volume) {
+        this.model = model;
+        this.size = size;
+        this.volume = volume;
+        //Объявление методов класса
+    }
+    @Override //переопределяем метод класса, чтобы его возвращаемые значения были читаемы
+    public String toString() {
+        return "Телевизор: " + model + ", диагональ: " + size + "\", громкость: " + volume;
+    }
+}
+class App { //Создаем класс App в котором и создадим при помощи сканнера экземпляр класса
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите модель: ");
+        String manualModel = scanner.nextLine();
+
+        System.out.print("Введите диагональ (дюймы): ");
+        double manualSize = scanner.nextDouble();
+
+        System.out.print("Введите громкость (0-100): ");
+        int manualVolume = scanner.nextInt();
+
+        Tv manualTv = new Tv(manualModel, manualSize, manualVolume);
+        System.out.println(manualTv);
+        scanner.close();
     }
 }
